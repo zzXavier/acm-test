96d437706770ddd862863f576a3f28c29b3fff9f
xavier05 <794853319@qq.com>
2025-06-18 16:48:11 +0800

    fix5: astro gblog

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,16 +19,16 @@ import url from 'node:url'
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 const alias = {
-    '@main': path.resolve(__dirname, 'src/main'),
-    '@src': path.resolve(__dirname, 'src'),
-    "@components": path.resolve(__dirname, 'src/components'),
-    '@content': path.resolve(__dirname, 'src/content'),
-    '@layouts': path.resolve(__dirname, 'src/layouts'),
-    '@scripts': path.resolve(__dirname, 'src/assets/scripts/'),
-    '@styles': path.resolve(__dirname, 'src/assets/styles'),
-    "@pages": path.resolve(__dirname, 'src/pages'),
-    '@support': path.resolve(__dirname, 'src/support'),
-    '@images': path.resolve(__dirname, 'src/images')
+    '@main': path.resolve(__dirname, './src/main'),
+    '@src': path.resolve(__dirname, './src'),
+    "@components": path.resolve(__dirname, './src/components'),
+    '@content': path.resolve(__dirname, './src/content'),
+    '@layouts': path.resolve(__dirname, './src/layouts'),
+    '@scripts': path.resolve(__dirname, './src/assets/scripts/'),
+    '@styles': path.resolve(__dirname, './src/assets/styles'),
+    "@pages": path.resolve(__dirname, './src/pages'),
+    '@support': path.resolve(__dirname, './src/support'),
+    '@images': path.resolve(__dirname, './src/images')
   }
 
 export default defineConfig({
